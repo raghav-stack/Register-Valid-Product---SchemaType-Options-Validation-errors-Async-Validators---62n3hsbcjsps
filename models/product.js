ff2ff7b435c7c703358457871c9855bcf8605057
Raghav <kumarraghav71950@gmail.com>
2023-05-13 18:51:47 +0530

    adding schema to models

--- models/product.js
+++ models/product.js
@@ -3,6 +3,26 @@ var mongoose = require("mongoose");
 //Complete Schema so that while ading new product it should be valid.
 // No two products can have same name in Document .
 var productSchema = mongoose.Schema({
+    name: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: async function(value) {
+        const productCount = await this.model('Product').countDocuments({ name: value });
+        return productCount === 0;
+      },
+      message: 'Product Name already exists'
+    }
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  }
   
 });
 
